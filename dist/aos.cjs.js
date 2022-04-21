0369ae22df1dc6e278dcb6faa83a65e40255d7fc
Dominiiiic <377457438@qq.com>
2022-04-21 16:34:56 +0800

    更新

--- dist/aos.cjs.js
+++ dist/aos.cjs.js
@@ -417,7 +417,7 @@ var elements = (function () {
 /* Clearing variables */
 
 var domList = {
-    'animateLoading1': '',
+    'animateLoading1': '<div class="animateLoading1Dom"><div></div><div></div><div></div><div></div></div>',
     'animateLoading2': '<div class="animateLoading2Dom"><div style="--i:0"></div><div style="--i:1"></div><div style="--i:2"></div><div style="--i:3"></div><div style="--i:4"></div></div>'
 };
 
